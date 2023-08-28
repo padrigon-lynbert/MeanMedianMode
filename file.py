9033874126db810c270f3587d0e4b30bb333028f
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2023-08-28 12:54:12 +0800

    get_input() created.

---
+++ file.py
@@ -0,0 +1,37 @@
+import os
+
+def cls(): os.system('cls')
+
+class Look:
+    def get_input():
+        cls()
+        data = list()
+        
+        while True:
+            cls()
+            while True:
+                input("<ENTER>"); cls()
+                print(f"Data: {data}")
+                print("<1> Add \n<2> Remove last index \n<3> change nothing and move on")
+                try:
+                    choice = int(input("\n\n:")); break
+                except ValueError: cls(); print("please input a valid number.")
+                
+            if choice in range(1,4):         
+                if choice == 1: 
+                    cls() 
+                    print("Separate the value using spaces.\n\n"); data = input("Add: ").split()
+                elif choice == 2: 
+                    try:
+                        data.pop()
+                    except IndexError: print("Catched no value to delete.")
+                else: break
+        
+Look.get_input()
+"""
+data = list()
+
+data = input("saparate: ")
+
+data_split = data.split()
+print(data_split)"""
