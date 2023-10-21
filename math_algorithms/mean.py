5f716188d1f71f38014c9136a52ab91664d62ae4
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-10-21 15:25:36 +0800

    +co-

--- math_algorithms/mean.py
+++ math_algorithms/mean.py
@@ -1,4 +1,4 @@
 def mean(given_list):
     result = sum(map(int,given_list))/len(given_list)
-    
+    #map: str to int (every index of the given_list), round: decimals
     return round(result, 4)
