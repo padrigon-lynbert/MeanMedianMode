b56ecd869f4b373d91b514aa883f6dedb1c11b2b
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-10-21 15:31:21 +0800

    +co-

--- math_algorithms/mode.py
+++ math_algorithms/mode.py
@@ -1,6 +1,7 @@
 from collections import Counter
 
-# data = [10,30,30,30,30,20,20,20,20]
+data = input(": ").split()
+
 def mode(given_lst):
 
     # Count repetition
@@ -11,6 +12,8 @@ def mode(given_lst):
     # Search multiple modes
     modes = [key for key, value in count.items() if value == max_count]
 
-    print(f"Mode(s): {modes}: {max_count} times")
+    return f"Mode(s): {modes}: {max_count} times"
 
-# mode(data)
+print(mode(data))
+
+
