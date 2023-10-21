4e7d920af3737226db73c3912ceed98e40d0e58a
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-10-21 18:33:47 +0800

    all ok

--- math_algorithms/mean.py
+++ math_algorithms/mean.py
@@ -2,10 +2,3 @@ def mean(given_list):
     result = sum(map(int,given_list))/len(given_list)
     #map: str to int (every index of the given_list), round: decimals
     return round(result, 4)
-
-# data = [1, 2, 3, 4, 5]
-
-# get_input = input(": ").split()
-# data = list(map(int, get_input))
-
-# print(mean(data))
