6cd816e0bf256139544f9781fa3fafb97a20bfcd
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-10-21 17:23:24 +0800

    median fixed.

--- math_algorithms/median.py
+++ math_algorithms/median.py
@@ -1,16 +1,8 @@
-def for_median(data):
-    # ( data[len(data)/2] + data[ ( (len(data)/2) +1) ] /2 )
+def for_median(given_list):
 
-    data.sort()
+    given_list.sort()
 
-    if (len(data) % 2 == 0): missing_median = (data[int(len(data)/2)] + data[int( (len(data)+1) /2) ]) / 2 #even
-    else: missing_median = data[int((len(data)+1)/2)] #odd
-        
-    return missing_median
-
-
-data = [3,2,1,5,4]
-
-c = for_median(data)
-print(c)
-# wrong output.
+    if (len(given_list)%2 == 0): median = ((len(given_list)/2) + ((len(given_list)/2)+1)) / 2 #even
+    else: median = (len(given_list) + 1) / 2 #odd
+    
+    return median
--- math_algorithms/mode.py
+++ math_algorithms/mode.py
@@ -1,7 +1,5 @@
 from collections import Counter
 
-data = input(": ").split()
-
 def mode(given_lst):
 
     # Count repetition
@@ -13,7 +11,5 @@ def mode(given_lst):
     modes = [key for key, value in count.items() if value == max_count]
 
     return f"Mode(s): {modes}: {max_count} times"
-
-print(mode(data))
 
 
