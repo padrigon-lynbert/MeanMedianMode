0cc6edfd3b7e35c12a890219828b7b973e3576c3
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-10-23 18:22:27 +0800

    float error fixed.

--- math_algorithms/mean.py
+++ math_algorithms/mean.py
@@ -1,4 +1,4 @@
 def mean(given_list):
     result = sum(map(float,given_list))/len(given_list)
-    #map: str to int (every index of the given_list), round: decimals
+    #map: str to float (every index of the given_list), round: decimals
     return round(result, 4)
