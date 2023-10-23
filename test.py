32ffe338455178c97796585f89cfb1b4f28f17fc
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-10-23 18:20:39 +0800

    float error fixed.

--- test.py
+++ test.py
@@ -9,3 +9,5 @@ get_input = input(": ").split()
 data = list(map(float, get_input))
 
 print(f"Mean: {mean(data)} \nMedian: {median(data)} \nMode: {mode(data)}")
+
+# Calculate the mean, median, and mode of a list using an easy-to-read input method. Already with a basic example. This is a very straightforward submodule.
